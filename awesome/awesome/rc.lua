526ab9a3d720a0cb31777b5000e764fb9379c412
William A. Kenningtonn III <william@wkennington.com>
2012-09-07 16:59:29 +0000

    Fix Theme

--- awesome/awesome/rc.lua
+++ awesome/awesome/rc.lua
@@ -35,7 +35,7 @@ end
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
 -- beautiful.init("/usr/share/awesome/themes/default/theme.lua")
-beautiful.init("theme.lua")
+beautiful.init("~/.config/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "terminal"
