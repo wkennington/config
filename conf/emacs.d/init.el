fd71092b35d7abf4ba30740463613cb8023c5000
William A. Kennington III <william@wkennington.com>
2013-08-27 19:33:06 -0500

    Fix SH Mode in Emacs

--- conf/emacs.d/init.el
+++ conf/emacs.d/init.el
@@ -153,3 +153,10 @@
 (setq wg-path "~/.emacs.d/workgroups")
 (when (file-exists-p wg-path)
   (wg-load wg-path))
+
+;; Indentation
+(defun setup-sh-mode ()
+  (interactive)
+  (setq sh-basic-offset 2
+        sh-indentation 2))
+(add-hook 'sh-mode-hook 'setup-sh-mode)
