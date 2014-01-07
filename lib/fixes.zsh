dc91457263ea8a92f404ce48dc3f717bbe9a3ba6
William A. Kennington III <william@wkennington.com>
2014-01-06 22:57:29 -0600

    Fix path_hasbin so that it works properly under zsh

--- lib/fixes.zsh
+++ lib/fixes.zsh
@@ -30,3 +30,9 @@ array_append () {
 array_size () {
   eval "echo \${#$1}"
 }
+
+# Tests to see if a binary exists in the path
+path_hasbin () {
+  [ "$#" -ne "1" ] && return 2
+  whence -p $1 >/dev/null 2>&1
+}
