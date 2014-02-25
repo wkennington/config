c198d3b6358e404e1d2f9c0a9eb604debc5e0c38
William A. Kennington III <william@wkennington.com>
2014-02-25 17:24:29 -0600

    Update nixpkgs

--- conf/nixpkgs/config.nix
+++ conf/nixpkgs/config.nix
@@ -13,12 +13,15 @@ pkgs : {
   packageOverrides = self : rec {
     hsEnv = self.haskellPackages.ghcWithPackages (self : with self; [
       xmonad
+      yi
     ]);
     graphical = self.buildEnv {
       name = "myGraphical";
       paths = with self; [
         # Envs
+        cs225
         dev
+        orpheum
         shell
 
         # Pkgs
@@ -36,7 +39,9 @@ pkgs : {
         pulseaudio
         scrot
         spotify
+        virtmanager
         vlc
+        weechat
         xfce.terminal
         xlibs.xbacklight
         xscreensaver
@@ -47,17 +52,18 @@ pkgs : {
       name = "myNonGraphical";
       paths = with self; [
         dev
+        dos2unix
+        #ffmpeg
         shell
+        zip
       ];
     };
     dev = self.buildEnv {
       name = "myDev";
       paths = with self; [
-        gdb
         mercurial
         python3Packages.ipython
         subversion
-        valgrind
       ];
     };
     shell = self.buildEnv {
@@ -74,6 +80,26 @@ pkgs : {
         unzip
         vim
         wget
+      ];
+    };
+    cs225 = self.myEnvFun {
+      name = "cs225";
+      buildInputs = with self; [
+        gdb
+        imagemagick
+        libpng
+        stdenv
+        valgrind
+      ];
+    };
+    orpheum = self.myEnvFun {
+      name = "orpheum";
+      buildInputs = with self; [
+        python27
+        python27Packages.django_1_5
+        python27Packages.gdata
+        python27Packages.pil
+        python27Packages.sqlite3
       ];
     };
   };
