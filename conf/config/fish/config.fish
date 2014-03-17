93d311daf653f02d0f595076ca077e89816610bd
William A. Kennington III <william@wkennington.com>
2014-03-17 10:30:49 -0500

    Fix tmp dir creation in fish

--- conf/config/fish/config.fish
+++ conf/config/fish/config.fish
@@ -15,8 +15,45 @@ function t
 	date
 end
 
+function symlink
+	mkdir -p (dirname "$argv[2]")
+	if [ (readlink -f "$argv[2]") != "$argv[1]" ]
+		rm -rf "$argv[2]"
+		ln -s "$argv[1]" "$argv[2]" ^/dev/null; or return 1
+	end
+end
+
+function dir_tmp
+	set DIR_TMP ""
+	dir_tmp_tryall
+	if [ "$DIR_TMP" = "" ]
+		echo "Failed to find a temporary folder" >&2
+		return 1;
+	end
+	[ ! -d "$DIR_TMP" ]; and mkdir "$DIR_TMP"; and chmod 0700 "$DIR_TMP"
+	symlink "$DIR_TMP" "$HOME/.tmp"
+	mkdir "$DIR_TMP/cache" >/dev/null ^&1
+	symlink "$DIR_TMP/cache" "$HOME/.cache"
+end
+
+function dir_tmp_tryall
+	dir_tmp_one "$ROOT/dev/shm"; and return 0
+	dir_tmp_one "$ROOT/run/shm"; and return 0
+	dir_tmp_one "$ROOT/tmp"; and return 0
+	dir_tmp_one "$ROOT/var/tmp"; and return 0
+	return 1
+end
+
+function dir_tmp_one
+  [ -w "$argv[1]" ]; or return 1
+  mount | grep '\(tmpfs\|ramfs\)' | grep $argv[1] >/dev/null; or return 1
+  set id (id -u)
+  set -g DIR_TMP "$argv[1]/tmp-$id"
+end
+
 set -x EDITOR vim
 set -x PAGER less
 set -x BLOCKSIZE M
 
 add_to_path "$HOME/.bin"
+dir_tmp
